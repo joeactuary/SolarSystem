982301a44919952a919b3f192f2fd8dd1a55241a
joeactuary <goldman256@gmail.com>
2021-11-07 00:23:51 -0400

    Solar System Instructer Base Bones

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SolarSystem andromeda =new SolarSystem();
+        andromeda.Sun=1;
+        andromeda.planet=2;
+        andromeda.moon=4;
+
+        featureSun mikesun = new featureSun();
+        mikesun.color="Red";
+        mikesun.heat="888881111111111111kj";
+        mikesun.temp="60000545454454545kelvin";
+        mikesun.radius="9000000km";
+        mikesun.maritalStatus="in love with asghar";
+
+    }
+}
